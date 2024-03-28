353c126418a63498e9176339c4d7fb2da6ad42bf
vicrdguez <contact@vicrodriguez.dev>
2024-03-28 21:10:30 +0400

    Fixed require pointing to parent project folder

--- nvim/ftplugin/nix.lua
+++ nvim/ftplugin/nix.lua
@@ -14,5 +14,5 @@ vim.lsp.start {
   name = 'nil_ls',
   cmd = { 'nil' },
   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
-  capabilities = require('user.lsp').make_client_capabilities(),
+  capabilities = require('lib.lsp').make_client_capabilities(),
 }
