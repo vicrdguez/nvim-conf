02d91dd99d5c6891f5906cb032caa1f27de699c7
vicrdguez <contact@vicrodriguez.dev>
2024-04-22 13:26:06 +0400

    cmp custom lspkind priority, completion pmenu follows typing

--- nvim/plugin/cmp.lua
+++ nvim/plugin/cmp.lua
@@ -2,6 +2,60 @@
 --
 local cmp = require('cmp')
 local luasnip = require('luasnip')
+local compare = require('cmp.config.compare')
+
+-- stolen from here: https://github.com/hrsh7th/nvim-cmp/issues/156#issuecomment-916338617
+-- I want to give Fields more priority than methods f.e. this should do the trick
+local kind_priority = {
+    Field = 11,
+    Property = 11,
+    Constant = 10,
+    Enum = 10,
+    EnumMember = 10,
+    Event = 10,
+    Function = 10,
+    Method = 10,
+    Operator = 10,
+    Reference = 10,
+    Struct = 10,
+    Variable = 9,
+    File = 8,
+    Folder = 8,
+    Class = 5,
+    Color = 5,
+    Module = 5,
+    Keyword = 2,
+    Constructor = 1,
+    Interface = 1,
+    Snippet = 0,
+    Text = 1,
+    TypeParameter = 1,
+    Unit = 1,
+    Value = 1,
+}
+
+local lspkind_comparator = function(kind_priority)
+    local lsp_types = require('cmp.types').lsp
+    return function(entry1, entry2)
+        if entry1.source.name ~= 'nvim_lsp' then
+            if entry2.source.name == 'nvim_lsp' then
+                return false
+            else
+                return nil
+            end
+        end
+        local kind1 = lsp_types.CompletionItemKind[entry1:get_kind()]
+        local kind2 = lsp_types.CompletionItemKind[entry2:get_kind()]
+
+        local priority1 = kind_priority[kind1] or 0
+        local priority2 = kind_priority[kind2] or 0
+        if priority1 == priority2 then
+            return nil
+        end
+        return priority2 < priority1
+    end
+end
+-- Theft end
 
 local function expand_jump_or_confirm()
     if luasnip.locally_jumpable(-1) then
@@ -72,4 +126,24 @@ cmp.setup {
             }
         })
     },
+    sorting = {
+        -- default cmp comparators, just changing 'kind' for the custom kind comparator
+        comparators = {
+            compare.offset,
+            compare.exact,
+            -- compare.scopes,
+            compare.score,
+            compare.recently_used,
+            compare.locality,
+            lspkind_comparator(kind_priority), --compare.kind,
+            -- compare.sort_text,
+            compare.length,
+            compare.order,
+        },
+    },
+    view = {
+        entries = {
+            follow_cursor = true
+        }
+    }
 }
