f02086c307c68006ad412d61ab482604d5caf5ee
vicrdguez <contact@vicrodriguez.dev>
2024-04-29 12:14:45 +0400

    changed lua indent options

--- nvim/ftplugin/lua.lua
+++ nvim/ftplugin/lua.lua
@@ -1,4 +1,9 @@
 vim.bo.comments = ':---,:--'
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 local lua_ls_cmd = 'lua-language-server'
 
